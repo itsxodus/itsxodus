b514922924ee77cbbcc1d3631da62414c526aab8
Matthew Lamour <68308905+itsxodus@users.noreply.github.com>
2024-03-14 19:23:35 -0400

    Add links to overview pages

--- app/(overview)/pc/page.tsx
+++ app/(overview)/pc/page.tsx
@@ -9,6 +9,9 @@ export default function Home() {
     <div className="p-4 m-4 text-4xl text-center text-white">
       PC Build content coming soon...
     </div>
+    <div className="p-4 m-4 text-2xl text-center text-white">
+      View my <a href="https://pcpartpicker.com/user/XoDusPC/" className="text-sky-500 underline">PCPartPicker</a> profile in the meantime!
+    </div>
     <Footer/>
     </>
   );
--- app/(overview)/projects/page.tsx
+++ app/(overview)/projects/page.tsx
@@ -9,6 +9,9 @@ export default function Home() {
     <div className="p-4 m-4 text-4xl text-center text-white">
       Project portfolio coming soon...
     </div>
+    <div className="p-4 m-4 text-2xl text-center text-white">
+      View my <a href="https://github.com/itsxodus/" className="text-sky-500 underline">GitHub</a> in the meantime!
+    </div>
     <Footer/>
     </>
   );
